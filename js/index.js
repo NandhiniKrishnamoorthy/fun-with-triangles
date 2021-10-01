29b9ca254b24d85e30ce4f1b86ee475460fd1cda
NandhiniKrishnamoorthy <nandhiniprasath25@gmail.com>
2021-09-30 19:30:57 -0700

    Docs: Added is_triangle functionality

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// JavaScript source code
+var angle1 = document.querySelector("#angle1");
+var angle2 = document.querySelector("#angle2");
+var angle3 = document.querySelector("#angle3");
+var isTriangle = document.querySelector("#check-button");
+var outputMessage = document.querySelector("#error-message");
+
+
+isTriangle.addEventListener("click", function check() {
+    var angle1Value = Number(angle1.value);
+    var angle2Value = Number(angle2.value);
+    var angle3Value = Number(angle3.value);
+
+    if (angle1Value && angle2Value && angle3Value) {
+        var sum = angle1Value + angle2Value + angle3Value;
+        if (sum === 180) {
+            showMessage("Success! these angles form a triangle!");
+        }
+        else {
+            showMessage("Failed! these angles don't form a triangle!");
+        }
+    }
+    else {
+        showMessage("Please enter all 3 angle values");
+    }
+});
+
+function showMessage(msg) {
+    outputMessage.innerText = msg;
+}
